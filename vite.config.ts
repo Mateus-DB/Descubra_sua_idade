b38522b9d2058adc274969c4f79c1db502fd2ee4
mateus <demartinomateus@gmail.com>
2025-05-24 02:42:52 -0300

    confirmado

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Descubra_sua_idade/'
+  base: '/Descubra_sua_idade'
 })
